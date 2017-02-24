fa3a49977afa103c8df89309ccd71dfa193a3409
linnal <erindajaupi@gmail.com>
2017-02-24 15:25:35 +0100

    add test for a single book in the shopping cart

--- PotterBookTdd/src/test/java/TestPriceCalculator.java
+++ PotterBookTdd/src/test/java/TestPriceCalculator.java
@@ -1,6 +1,8 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+
+
 public class TestPriceCalculator {
 
     @Test
@@ -11,5 +13,17 @@ public class TestPriceCalculator {
         float price = priceCalculator.price();
 
         Assert.assertEquals(0, price, 0.0f);
+    }
+
+    @Test
+    public void shouldReturnRealPriceForASingleBook(){
+        ShoppingBasket shoppingBasket = new ShoppingBasket();
+        PriceCalculator priceCalculator = new PriceCalculator(shoppingBasket);
+
+        shoppingBasket.add(new Book());
+
+        float price = priceCalculator.price();
+
+        Assert.assertEquals(8, price, 0.0f);
     }
 }
