33cf8c1cf25b7dec77aa336fdb19a1ab6d7f94a3
linnal <erindajaupi@gmail.com>
2017-02-24 15:22:55 +0100

    make first test pass

---
+++ PotterBookTdd/src/main/java/PriceCalculator.java
@@ -0,0 +1,14 @@
+/**
+ * Created by linnal on 2/24/17.
+ */
+public class PriceCalculator {
+
+    public PriceCalculator(ShoppingBasket shoppingBasket) {
+
+    }
+
+    public float price() {
+        return 0;
+    }
+
+}
