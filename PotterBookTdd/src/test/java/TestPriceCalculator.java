2717dfaba0473e218cc45b2cc0a38a2880e7a6ff
linnal <erindajaupi@gmail.com>
2017-02-24 15:52:56 +0100

    test: add same book twice should return 16

--- PotterBookTdd/src/test/java/TestPriceCalculator.java
+++ PotterBookTdd/src/test/java/TestPriceCalculator.java
@@ -24,4 +24,18 @@ public class TestPriceCalculator {
 
         Assert.assertEquals(8, price, 0.0f);
     }
+
+    @Test
+    public void addSameBookTwiceIntoTheBasket(){
+        ShoppingBasket shoppingBasket = new ShoppingBasket();
+        PriceCalculator priceCalculator = new PriceCalculator(shoppingBasket);
+
+        Book book = new Book("first");
+        shoppingBasket.add(book);
+        shoppingBasket.add(book);
+
+        float price = priceCalculator.price();
+
+        Assert.assertEquals(16, price, 0.00f);
+    }
 }
