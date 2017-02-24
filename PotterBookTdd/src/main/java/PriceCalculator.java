c4cbbde2a0932df74b2bc5a159a0c862b55a62fe
linnal <erindajaupi@gmail.com>
2017-02-24 16:55:02 +0100

    test: to calculate discount shopping basket should be able to count distinct books

--- PotterBookTdd/src/main/java/PriceCalculator.java
+++ PotterBookTdd/src/main/java/PriceCalculator.java
@@ -13,8 +13,17 @@ public class PriceCalculator {
         if(shoppingBasket.isEmpty()) {
             return 0;
         }else{
-            return 8 * shoppingBasket.size();
+            float price = 8 * shoppingBasket.size();
+            float discount = 0;
+            if(shoppingBasket.countDistinctBooks() > 0){
+                discount = calculateDiscount(price);
+            }
+            return price - discount;
         }
+    }
+
+    private float calculateDiscount(float price){
+        return price * 5f/100f;
     }
 
 }
