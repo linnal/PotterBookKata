df7ff3aa80eb45a7b13c2a8a71c96d286d084e8e
linnal <erindajaupi@gmail.com>
2017-02-24 15:36:13 +0100

    make shoppingBasketIsEmpty pass

--- PotterBookTdd/src/main/java/ShoppingBasket.java
+++ PotterBookTdd/src/main/java/ShoppingBasket.java
@@ -1,8 +1,9 @@
-
-/**
- * Created by linnal on 2/24/17.
- */
 public class ShoppingBasket {
+
     public void add(Book book) {
+    }
+
+    public boolean isEmpty() {
+        return true;
     }
 }
